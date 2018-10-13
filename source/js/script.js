00e750987186845343742306e7fbd1bfd10f0393
Mikhail Shardin <kriptonit1988@gmail.com>
2018-10-14 00:00:43 +0500

    Добавил скрипты для открытия модального окна

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,39 @@
 var navigation = document.querySelector(".navigation");
 var button = navigation.querySelector(".navigation__button");
+var weekOrder = document.querySelector(".week__order");
+var goodsButtons = document.querySelectorAll(".goods__cart-button");
+var modal = document.querySelector(".modal");
+var overlay = document.querySelector(".modal__overlay");
 
 navigation.classList.remove("navigation--nojs");
 navigation.classList.add("navigation--closed");
 
 button.addEventListener('click', function() {
   navigation.classList.toggle("navigation--closed");
+});
+
+if (weekOrder) {
+  weekOrder.addEventListener('click', function() {
+    modal.classList.add("modal--show");
+    overlay.classList.add("modal__overlay--show");
+  });
+}
+
+if (goodsButtons) {
+  for (var i = 0; i < goodsButtons.length; i++) {
+    goodsButtons[i].addEventListener('click', function() {
+      modal.classList.add("modal--show");
+      overlay.classList.add("modal__overlay--show");
+    });
+  }
+}
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modal.classList.contains("modal--show")) {
+      evt.preventDefault();
+      modal.classList.remove("modal--show");
+      overlay.classList.remove("modal__overlay--show");
+    }
+  }
 });
