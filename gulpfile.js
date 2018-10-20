0bcd3806851792f75fcffa421103afcd95451b52
Mikhail Shardin <kriptonit1988@gmail.com>
2018-10-21 02:01:44 +0500

    Временно добавил pixel-glass в build

--- gulpfile.js
+++ gulpfile.js
@@ -66,7 +66,8 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js/**"
+    "source/js/**",
+    "source/pixel-glass/**"
   ], {
     base: "source"
   })
