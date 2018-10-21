c35fbd93ec84ee0c90301072d738ecc73aa6bdfc
Mikhail Shardin <kriptonit1988@gmail.com>
2018-10-21 10:41:26 +0500

    Исправил подключение pixel-glass

--- gulpfile.js
+++ gulpfile.js
@@ -66,8 +66,7 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js/**",
-    "source/pixel-glass/**"
+    "source/js/**"
   ], {
     base: "source"
   })
