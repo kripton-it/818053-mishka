560f9e0600bd2e4bc77de14541c3aefb3e780d43
Mikhail Shardin <kriptonit1988@gmail.com>
2018-10-26 13:19:05 +0500

    Вынес overlay в отдельный БЭМ-блок

--- source/js/script.js
+++ source/js/script.js
@@ -3,7 +3,7 @@ var button = navigation.querySelector(".navigation__button");
 var weekOrder = document.querySelector(".week__order");
 var goodsButtons = document.querySelectorAll(".goods__cart-button");
 var modal = document.querySelector(".modal");
-var overlay = document.querySelector(".modal__overlay");
+var overlay = document.querySelector(".overlay");
 
 navigation.classList.remove("navigation--nojs");
 navigation.classList.add("navigation--closed");
@@ -15,7 +15,7 @@ button.addEventListener('click', function () {
 if (weekOrder) {
   weekOrder.addEventListener('click', function () {
     modal.classList.add("modal--show");
-    overlay.classList.add("modal__overlay--show");
+    overlay.classList.add("overlay--show");
   });
 }
 
@@ -23,7 +23,7 @@ if (goodsButtons) {
   for (var i = 0; i < goodsButtons.length; i++) {
     goodsButtons[i].addEventListener('click', function () {
       modal.classList.add("modal--show");
-      overlay.classList.add("modal__overlay--show");
+      overlay.classList.add("overlay--show");
     });
   }
 }
@@ -33,12 +33,12 @@ window.addEventListener("keydown", function (evt) {
     if (modal.classList.contains("modal--show")) {
       evt.preventDefault();
       modal.classList.remove("modal--show");
-      overlay.classList.remove("modal__overlay--show");
+      overlay.classList.remove("overlay--show");
     }
   }
 });
 
 overlay.addEventListener("click", function () {
   modal.classList.remove("modal--show");
-  overlay.classList.remove("modal__overlay--show");
+  overlay.classList.remove("overlay--show");
 });
